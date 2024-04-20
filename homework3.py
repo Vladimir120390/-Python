af0ee5d1d37ca4bf43b2a6e0ebab0042c00d719c
Vladimir120390 <166973041+Vladimir120390@users.noreply.github.com>
2024-04-20 22:24:33 +0400

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,16 @@
+my_string = input("Введите данные: ")
+print("Длина строки: ", len(my_string))
+print("Строка my_string в верхнем регистре: ", my_string.upper())
+print("Строка my_string в нижнем регистре: " , my_string.lower())
+my_string_no_spaces = my_string.replace(" ", "")
+print("Строка без пробелов: ", my_string_no_spaces)
+print("Первый символ строки my_string: ", my_string[0])
+print("Последний символ строки my_string: ",  my_string[-1])
+
+
+
+
+
+
+
+
