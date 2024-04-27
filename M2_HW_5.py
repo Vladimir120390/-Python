fe1a2f8738566323b56994059a99a3f35f2ba657
Vladimir120390 <166973041+Vladimir120390@users.noreply.github.com>
2024-04-27 09:33:48 +0400

    Update M2_HW_5.py

--- M2_HW_5.py
+++ M2_HW_5.py
@@ -1,8 +1,6 @@
 car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-for i in car:
-    print('Я езжу на автомобиле марки', i)
 car_count = 0
 for i in car:
+    print('Я езжу на автомобиле марки', i)
     car_count += 10
-    print('всего машин', car_count)
-print("cars_count =", car_count)
+    print('У меня этих машин - ', car_count, "шт")
