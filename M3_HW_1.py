1c00b95a39176c8547def84ed12fc9049ece7e89
Vladimir120390 <166973041+Vladimir120390@users.noreply.github.com>
2024-04-29 17:58:45 +0400

    Update M3_HW_1.py

--- M3_HW_1.py
+++ M3_HW_1.py
@@ -1,13 +1,13 @@
 def test(a, b):
     print(a, b)
 test(1,2)
-print()
+print()# для лучшей читаемости))
 
 
 def test2(a, *, b=True, c=1):# вариант № 1
     print(a,b,c)
 test2(a="OK")
-print()
+print()# для лучшей читаемости))
 
 
 name = input("Введите имя: ") # вариант № 2
