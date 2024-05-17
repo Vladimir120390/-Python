3827dfde4a5128e38dbf9e36e3db49220b17f6d3
Vladimir120390 <166973041+Vladimir120390@users.noreply.github.com>
2024-05-17 13:27:11 +0400

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -3,7 +3,7 @@ n = int(input("Введите число от 3 до 20: "))
 result = ''
 for i in range(1, n):
     for j in range(1, 21):
-        if i + j == n and n % (i + j) == 0:
+        if i + j == n and n % (i + j) == 0 and i < j:
             result += str(i) + str(j)
 
 print(result)
