6bb2c52f3ae5b1fb19ca04d013f9aeb7c00bf84a
Vladimir120390 <166973041+Vladimir120390@users.noreply.github.com>
2024-08-09 17:00:26 +0400

    Add files via upload

---
+++ M10_2_HW_1_Многопроцессное программирование.py
@@ -0,0 +1,29 @@
+import os
+import time
+from multiprocessing import Pool
+
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as file:
+        while True:
+            line = file.readline().strip()
+            if not line:
+                break
+            all_data.append(line)
+
+
+if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+    # Линейный вызов
+    start_time = time.time()
+    for filename in filenames:
+        read_info(filename)
+    print(f"Время выполнения линейного подхода: {time.time() - start_time}")
+
+    # Многопроцессный
+    start_time = time.time()
+    with Pool(processes=4) as pool:
+        pool.map(read_info, filenames)
+    print(f"Время выполнения многопроцессного подхода: {time.time() - start_time}")
