3436558c1e58b7c173b6ec512c9aa646284c8110
Vladimir120390 <166973041+Vladimir120390@users.noreply.github.com>
2024-08-24 12:46:01 +0400

    Add files via upload

---
+++ M12_HW_4_Логирование.py
@@ -0,0 +1,64 @@
+import unittest
+import logging
+
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,
+    filename='runner_tests.log',
+    filemode='w',
+    encoding='utf-8',
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+# Класс Runner
+class Runner:
+    def __init__(self, name, speed=5):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
+        self.distance = 0
+        if speed > 0:
+            self.speed = speed
+        else:
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+# Тесты для Runner
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        try:
+            runner = Runner("Test Runner", -5)  # Передаем отрицательное значение в speed
+            runner.walk()
+            logging.info('"test_walk" выполнен успешно')
+        except ValueError as e:
+            logging.warning("Неверная скорость для Runner: %s", e)
+
+    def test_run(self):
+        try:
+            runner = Runner(123, 10)  # Передаем неверный тип в name
+            runner.run()
+            logging.info('"test_run" выполнен успешно')
+        except TypeError as e:
+            logging.warning("Неверный тип данных для объекта Runner: %s", e)
+
+if __name__ == '__main__':
+    unittest.main()
