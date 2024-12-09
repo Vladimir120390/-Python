2474ccfab9cdf292208274c4a32a7a35df0a0279
Vladimir120390 <166973041+Vladimir120390@users.noreply.github.com>
2024-12-09 18:53:01 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -11,10 +12,23 @@ def read_admin():
     return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-def read_user(user_id: int):
+def read_user(
+    user_id: Annotated[int, Path(
+        ge=1, le=100, description="Enter User ID", example=1
+    )]
+):
     return f"Вы вошли как пользователь № {user_id}"
 
-@app.get("/user")
-def read_user_info(username: str, age: int):
+@app.get("/user/{username}/{age}")
+def read_user_info(
+    username: Annotated[str, Path(
+        min_length=5, max_length=20, description="Enter username", example="UrbanUser"
+    )],
+    age: Annotated[int, Path(
+        ge=18, le=120, description="Enter age", example=24
+    )]
+):
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
+
+
 
