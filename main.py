d7e7cdc9ef12f3e01cea1e4b98edc3e77bf63f0b
Vladimir120390 <166973041+Vladimir120390@users.noreply.github.com>
2024-12-28 15:43:11 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,14 @@
-from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
-
+from fastapi import FastAPI
+from app.routers import task, user
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
-# Модель пользователя
-class User(BaseModel):
-    id: int
-    username: str
-    age: int
+@app.get("/", tags=["welcome"])
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
 
-# Список пользователей (вместо базы данных)
-users = [
-    User(id=1, username="UrbanUser", age=24),
-    User(id=2, username="UrbanTest", age=22),
-    User(id=3, username="Capybara", age=60),
-]
+# Подключаем маршруты
+app.include_router(user.router)
+app.include_router(task.router)
 
-# Главная страница, отображающая список пользователей
-@app.get("/", response_class=HTMLResponse)
-async def read_users(request: Request):
-    return templates.TemplateResponse("users.html", {"request": request, "users": users})
-
-# Страница пользователя по ID
-@app.get("/user/{user_id}", response_class=HTMLResponse)
-async def read_user(request: Request, user_id: int):
-    user = next((user for user in users if user.id == user_id), None)
-    if user is None:
-        return templates.TemplateResponse("404.html", {"request": request})
-    return templates.TemplateResponse("users.html", {"request": request, "user": user})
-
-# Удаление пользователя
-@app.delete("/user/{user_id}")
-async def delete_user(user_id: int):
-    global users
-    user = next((user for user in users if user.id == user_id), None)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    users = [user for user in users if user.id != user_id]
-    return {"message": "User deleted", "user_id": user_id}
-
-# Обновление пользователя
-@app.put("/user/{user_id}")
-async def update_user(user_id: int, user: User):
-    for index, existing_user in enumerate(users):
-        if existing_user.id == user_id:
-            users[index] = user
-            return {"message": "User updated", "user": user}
-    raise HTTPException(status_code=404, detail="User not found")
-
-# Создание пользователя
-@app.post("/user/")
-async def create_user(user: User):
-    users.append(user)
-    return user
 
